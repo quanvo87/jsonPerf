d716ffce72d6721451d100d9f9edb14c72580ecd
Quan Vo <quan.vo@ibm.com>
2017-07-24 16:20:02 -0500

    Remove SwiftyJSON, update to 7/23 snapshot

--- Tests/jsonPerfTests/LargeTests.swift
+++ Tests/jsonPerfTests/LargeTests.swift
@@ -55,3 +55,11 @@ extension LargeTests {
         XCTAssert(large?.programList.programs.count ?? 0 == 4710)
     }
 }
+
+let largeData: Data = {
+    let path = URL(fileURLWithPath: #file).appendingPathComponent("../large.json").standardized.path
+    let file = FileHandle(forReadingAtPath: path)!
+    let data = file.readDataToEndOfFile()
+    file.closeFile()
+    return data
+}()
